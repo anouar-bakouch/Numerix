e6af95791f1f89fcb24b7ed515b13ba45429e7f4
anouar <anouar.bakouch@usmba.ac.ma>
2023-04-24 14:31:22 +0100

    updated Main.js

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,6 @@
 // import All the methods
-
 import determinant from "./determinant.js";
 import inverse from "./inverse.js"
-
 
 const ctx = document.getElementById('myChart');
 
@@ -16,33 +14,32 @@ let simpleMethods = ["determinant","inverse"]
 let nSelect = document.createElement("select"); 
 nSelect.id = "mathMet";
 let select = document.getElementById("select");
-let matrix = [];
+let matrix;
+let r = 0;
 
-let init = ()=>{
-    
-    let selected = document.getElementById("select").value;
+let init = (selected) => {
     let div_new = document.createElement("div");
     div_new.id = "form";
 
-        for(let j = 0;j<selected;j++){
-            // create div based on the selected value
-            let div = document.createElement("div");
-            div.id = "div" + j;
-            div_new.appendChild(div);
-            // create input based on the selected * selected value
-            for (let i = 0; i < selected; i++) {
-                let input = document.createElement("input");
-                input.type = "number";
-                input.min = 0;
-                input.max = 100;
-                input.name = "input" + i;
-                input.id = "input" + i;
-                div.appendChild(input);
-                document.getElementById("matrix").appendChild(div);
-                div_new.appendChild(div);
-            }
-    }  
-    document.getElementById("matrix").appendChild(div_new);
+    for (let j = 0; j < selected; j++) {
+        let div = document.createElement("div");
+        div.id = "div" + j;
+        div_new.appendChild(div);
+
+        for (let i = 0; i < selected; i++) {
+            let input = document.createElement("input");
+            input.type = "number";
+            input.min = 0;
+            input.max = 100;
+            input.name = "input" + i;
+            input.id = "input" + i;
+            input.addEventListener("change", () => {
+                matrix[j][i] = Number(input.value);
+            })
+            div.appendChild(input);
+        }
+        document.getElementById("matrix").appendChild(div_new);
+    }
 
     document.getElementById("form").appendChild(nSelect);
     let integrationMethodsOptGroup = document.createElement("optgroup");
@@ -58,82 +55,84 @@ let init = ()=>{
     let simpleMethodsOptGroup = document.createElement("optgroup");
     simpleMethodsOptGroup.label = "methodes simples"
 
-        // Fill each optgroup element with its corresponding array
-        integrationMethods.forEach((method) => {
-            let option = document.createElement("option");
-            option.value = method;
-            option.textContent = method;
-            integrationMethodsOptGroup.appendChild(option);
-        });
+    // Fill each optgroup element with its corresponding array
+    integrationMethods.forEach((method) => {
+        let option = document.createElement("option");
+        option.value = method;
+        option.textContent = method;
+        integrationMethodsOptGroup.appendChild(option);
+    });
 
-        interpolationMethods.forEach((method) => {
-            let option = document.createElement("option");
-            option.value = method;
-            option.textContent = method;
-            interpolationMethodsOptGroup.appendChild(option);
-        });
+    interpolationMethods.forEach((method) => {
+        let option = document.createElement("option");
+        option.value = method;
+        option.textContent = method;
+        interpolationMethodsOptGroup.appendChild(option);
+    });
 
-        methodesDivisees.forEach((method) => {
-            let option = document.createElement("option");
-            option.value = method;
-            option.textContent = method;
-            methodesDirectesOptGroup.appendChild(option);
-        });
+    methodesDivisees.forEach((method) => {
+        let option = document.createElement("option");
+        option.value = method;
+        option.textContent = method;
+        methodesDirectesOptGroup.appendChild(option);
+    });
 
-        methodesIteratives.forEach((method) => {
-            let option = document.createElement("option");
-            option.value = method;
-            option.textContent = method;
-            methodesIterativesOptGroup.appendChild(option);
-        });
+    methodesIteratives.forEach((method) => {
+        let option = document.createElement("option");
+        option.value = method;
+        option.textContent = method;
+        methodesIterativesOptGroup.appendChild(option);
+    });
 
-        methodesNonLineaires.forEach((method) => {
-            let option = document.createElement("option");
-            option.value = method;
-            option.textContent = method;
-            methodesNonLineairesOptGroup.appendChild(option);
-        });
+    methodesNonLineaires.forEach((method) => {
+        let option = document.createElement("option");
+        option.value = method;
+        option.textContent = method;
+        methodesNonLineairesOptGroup.appendChild(option);
+    });
 
-        simpleMethods.forEach((method) => {
-            let option = document.createElement("option");
-            option.value = method;
-            option.textContent = method;
-            simpleMethodsOptGroup.appendChild(option);
-        });
+    simpleMethods.forEach((method) => {
+        let option = document.createElement("option");
+        option.value = method;
+        option.textContent = method;
+        simpleMethodsOptGroup.appendChild(option);
+    });
 
-        // Append all optgroups to the select element
-        nSelect.appendChild(integrationMethodsOptGroup);
-        nSelect.appendChild(interpolationMethodsOptGroup);
-        nSelect.appendChild(methodesDirectesOptGroup);
-        nSelect.appendChild(methodesIterativesOptGroup);
-        nSelect.appendChild(methodesNonLineairesOptGroup);
-        nSelect.appendChild(simpleMethodsOptGroup);
+    // Append all optgroups to the select element
+    nSelect.appendChild(integrationMethodsOptGroup);
+    nSelect.appendChild(interpolationMethodsOptGroup);
+    nSelect.appendChild(methodesDirectesOptGroup);
+    nSelect.appendChild(methodesIterativesOptGroup);
+    nSelect.appendChild(methodesNonLineairesOptGroup);
+    nSelect.appendChild(simpleMethodsOptGroup);
 
-        for (let j = 0; j < selected; j++) { 
-            for (let i = 0; i < matrix[j].length; i++) {
-              matrix[j][i] = Number(document.getElementById(`input${i}`).value);
-            }
-        }
+    matrix = new Array(selected).fill(null).map(() => new Array(selected).fill(0));
+    r = selected;
 }
 
-
-
-select.addEventListener("change", ()=>{
+select.addEventListener("change", () => {
     let form = document.getElementById("form");
     let select = document.getElementById("mathMet");
-    if(form || select)  {
+
+    if (form || select)  {
         form.remove(); 
         select.remove();
     }
-    init();
+
+    const selected = document.getElementById("select").value;
+    init(selected);
 });
 
-document.getElementById("mathMet").addEventListener('change',()=>{
-
-    // Get the selected value from the <select> element
+document.getElementById("mathMet").addEventListener('change', () => {
     const selectedMethod = document.getElementById("mathMet").value;
 
-    // Check the selected method and call the corresponding function
+    for (let j = 0; j < r; j++) {
+        for (let i = 0; i < r; i++) {
+            let input = document.getElementById(`input${i}`);
+            matrix[j][i] = Number(input.value);
+        }
+    }
+
     switch(selectedMethod) {
         case "Simpson":
             // call Simpson function
@@ -185,5 +184,4 @@ document.getElementById("mathMet").addEventListener('change',()=>{
             console.log("Unknown method selected.");
             break;
     }
-})
-
+});
